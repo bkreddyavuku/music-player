ce468817aa24efb8ec0a1701925272971cb63179
bkreddyavuku <bklovebabu@gmail.com>
2024-05-26 22:09:15 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+      name: "Cheppave Chirugali",
+      artist: "Udit Narayan & Sujatha",
+      img: "okkadu",
+      src: "okkadu"
+    },
+    {
+      name: "Badtameez Dil",
+      artist: "Pritam , Benny Dayal & Shefali Alvares",
+      img: "hjyd",
+      src: "hjyd"
+    },
+    {
+      name: "Hukum",
+      artist: "Anirudh Ravichander , Dinker Kalavala & Bhaskarabhatla Ravi Kumar",
+      img: "jailer",
+      src: "jailer"
+    },
+    {
+      name: "Nammavemo",
+      artist: "Saketh",
+      img: "parugu",
+      src: "parugu"
+    },
+    {
+      name: "Safari",
+      artist: "Serena",
+      img: "serena",
+      src: "serena"
+    },
+    {
+      name: "Param Sundari",
+      artist: "A.R.Rahman & Shreya Ghoshal",
+      img: "mimi",
+      src: "mimi"
+    },
+  ];
